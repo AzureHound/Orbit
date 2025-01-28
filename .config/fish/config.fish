7b88bc0ea213284dc8e78842ff71d27655eebe30
Wolf <azurehound.landline090@slmail.me>
2025-01-28 07:28:07 +0530

    ⚙️ config[hypr]: Fixed Audio & Keybinds; Modified Other Changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -52,6 +52,7 @@ alias tree='eza -a -T --git-ignore --icons'
 alias lta4="eza -lTag --git-ignore --level=4 --icons"
 alias rm='trash put'
 alias rg='rg -i'
+alias ssh="kitten ssh"
 alias branch='git branch --sort=-committerdate | fzf --header "Checkout Recent Branch" --preview "git diff --color=always {1} | delta" --pointer="" | xargs git checkout'
 alias preview='kitten icat'
 alias gen='tgpt -i'
