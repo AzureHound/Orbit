f8f735373512e31e9fbba1ccec36895778d3a3d5
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-02-28 20:35:18 +0530

    🔧 chore: mionor changes

--- .config/fish/user_variables.fish
+++ .config/fish/user_variables.fish
@@ -51,21 +51,23 @@ fish_add_path $XDG_DATA_HOME/pnpm
 # Editor
 set -xg EDITOR nvim
 set -xg VISUAL $EDITOR
-set -xg SUDO_EDITOR $EDITOR
+set -xg SUDO_EDITOR vim
 
 # GPG/LANG
 set -xg GPG_TTY (tty)
 
 # FZF
 set -xg FZF_DEFAULT_COMMAND fd
-set -xg FZF_DEFAULT_OPTS "--height=90% --layout=reverse --info=inline --border rounded --pointer='' --margin=1 --padding=1 \
+set -xg FZF_DEFAULT_OPTS "--height=90% --layout=reverse --info=inline --border rounded --pointer='' --marker '⇒' --margin=1 --padding=1 \
 --color=bg+:-1,gutter:-1,spinner:#f4dbd6,hl:#ed8796 \
 --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6 \
 --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796 \
 --color=selected-bg:#494d64 \
 --bind 'ctrl-u:preview-half-page-up'
 --bind 'ctrl-d:preview-half-page-down'
---bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)'"
+--bind 'ctrl-y:execute-silent(printf {} | cut -f 2- | wl-copy --trim-newline)'
+--multi"
+
 set -xg fzf_preview_dir_cmd eza --long --header --icons --all --color=always --group-directories-first --hyperlink
 set -xg fzf_fd_opts --hidden --color=always
 set -xg _ZO_FZF_OPTS $FZF_DEFAULT_OPTS '--preview "{$fzf_preview_dir_cmd} {2}"'
@@ -78,4 +80,9 @@ set MANPATH $NPM_PACKAGES/share/man $MANPATH
 ##
 set -x MANPATH /usr/share/man:/usr/local/man:/usr/local/share/man
 # set PATH $PATH /home/eyes/Developer/repos/Orbit/.local/bin
+
+# Other
+if type -q vivid
+    set -xg LS_COLORS (vivid generate catppuccin-macchiato)
+end
 set -xg STARSHIP_LOG error
