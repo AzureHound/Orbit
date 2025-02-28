41279f0996b0a085c19acaa24fb0270f65f1fbcc
Wolf <azurehound.landline090@slmail.me>
2025-02-28 22:28:22 +0530

    🔧 chore: maintenance

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,7 +22,7 @@ if status is-interactive
   set fish_cursor_visual      line    blink
 
   function fish_user_key_bindings
-    # fish_default_key_bindings -M insert
+    fish_default_key_bindings -M insert
     fish_vi_key_bindings --no-erase insert
   end
 end
