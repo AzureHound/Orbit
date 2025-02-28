0f59ee057186ee3c26f3c6dd5838d9e8841218a4
Wolf <azurehound.landline090@slmail.me>
2025-02-28 21:42:08 +0530

    ♻️ refactor(fish): added aliases.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 source ~/.config/fish/user_variables.fish
 source ~/.config/fish/abbreviations.fish
+source ~/.config/fish/aliases.fish
 
 fish_config theme choose "Catppuccin Macchiato" # Catppuccin Macchiato, Dracula Official
 
@@ -62,39 +63,6 @@ end
 
 # zoxide
 zoxide init --cmd cd fish | source
-
-# aliasis
-alias la='eza -a --icons'
-alias ls='eza --icons'
-alias ll='eza -a -l --icons'
-alias cp='cp -i'
-alias mv='mv -i'
-alias df='df -h'
-alias tree='eza -a -T --git-ignore --icons'
-alias lta4="eza -lTag --git-ignore --level=4 --icons"
-alias histwipe="cliphist wipe"
-alias private='fish -P'
-alias rg='rg -i'
-
-alias asciiquarium='asciiquarium -t'
-alias bonsai='cbonsai --seed 119'
-alias colors='~/.local/bin/colorscript -r'
-alias clock='tty-clock -DScC6b'
-alias emoji='~/.local/bin/emojifzf'
-alias lofi='lowfi'
-alias preview='kitten icat'
-alias ani-down='~/.local/bin/ani-down'
-alias words='tt -theme ~/.tt/Catppuccin-Macchiato'
-alias matrix="unimatrix -n -c yellow -s 90 -l 'o'"
-alias weather='curl "v2.wttr.in/Agartala?F"'
-
-alias branch='git branch --sort=-committerdate | fzf --header "Checkout Recent Branch" --preview "git diff --color=always {1} | delta" --pointer="" | xargs git checkout'
-alias commits="~/.local/bin/commits"
-alias glog='git log --oneline --graph --all'
-alias gen='tgpt -i'
-alias ssh="kitten ssh"
-alias powertop='sudo powertop'
-alias zed='zeditor'
 
 # Keybinds
 bind \ce 'emoji'
