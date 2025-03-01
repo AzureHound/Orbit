2f3f2b5f572158e8be0d7e0c2005743b1df247b0
Wolf <azurehound.landline090@slmail.me>
2025-03-01 22:31:06 +0530

    🔧 chore: maintenance

--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -1,18 +1,13 @@
-# Function to define the left prompt (character module).
+# Left prompt
 function starship_transient_prompt_func
   starship module character
 end
 
-# Function to define the right prompt (time module).
+# Right prompt
 function starship_transient_rprompt_func
   starship module time
 end
 
-# Set the custom Starship configuration file.
-export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
-
 # Initialize Starship for Fish shell.
 starship init fish | source
-
-# Enable transience (if desired).
 enable_transience
