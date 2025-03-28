06d25531d8ab79dd6020e1ecb6a8ee8037e548fd
Wolf <azurehound.landline090@slmail.me>
2025-03-29 03:28:57 +0530

    ⚙️ config: kitten ssh

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -11,7 +11,7 @@ alias private='fish -P'
 alias rg='rg -i'
 alias glog='git log --oneline --graph --all'
 alias gen='tgpt -i'
-alias ssh="kitten ssh"
+alias ssh="kitty +kitten ssh"
 alias powertop='sudo powertop'
 alias zed='zeditor'
 
