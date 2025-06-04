102d74d5cd0dd3980f9c08a2d390d67a3ca8756b
Wolf <azurehound.landline090@slmail.me>
2025-06-04 13:44:01 +0530

    ♻️ refactor: Code refactoring

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -1,2 +1,4 @@
 abbr -a c clear
+abbr -a check pacman -Qs
 abbr -a d docker
+abbr -a v nvim
--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,25 +1,30 @@
 # aliasis
-alias .='cd ..'
-alias ..='cd ../..'
-alias ...='cd ../../..'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
 
 alias la='eza -a --icons'
+alias ls='eza --icons'
 alias ll='eza -a -l --icons'
 alias cp='cp -i'
 alias mv='mv -i'
+alias rg='rg -i'
 alias df='df -h'
+alias mkdir='mkdir -p'
 alias tree='eza -a -T --git-ignore --icons'
 alias lta4="eza -lTag --git-ignore --level=4 --icons"
-alias histwipe="cliphist wipe"
-alias private='fish -P'
-alias rg='rg -i'
-alias glog='git log --oneline --graph --all'
-alias gen='tgpt -i'
-alias powertop='sudo powertop'
-alias zed='zeditor'
+alias glogs='git log --oneline --graph --all'
+# alias branch='git branch --sort=-committerdate | fzf --header "Checkout Recent Branch" --preview "git diff --color=always {1} | delta" --pointer="" | xargs git checkout'
 
 alias asciiquarium='asciiquarium -t'
 alias bonsai='cbonsai --seed 119'
+alias clock='tty-clock -DScC6b'
+alias gen='tgpt -i'
+alias histwipe="cliphist wipe"
 alias lofi='lowfi'
+alias matrix="unimatrix -n -c yellow -s 90 -l 'o'"
+alias powertop='sudo powertop'
+alias private='fish -P'
 alias preview='kitten icat'
 alias words='tt -theme $HOME/.tt/Catppuccin-Macchiato'
+alias zed='zeditor'
