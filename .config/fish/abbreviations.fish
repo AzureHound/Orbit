ac6a5b764ee254f56e333263b30300c703817a69
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-07-15 01:03:51 +0530

    ♻️ refactor(fish): letter case changed

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -2,5 +2,5 @@ abbr -a c clear
 abbr -a check pacman -Qs
 abbr -a d docker
 abbr -a gamedir cd /mnt/Drive/SteamLibrary/steamapps/common
-abbr -a aio 'sudo liquidctl --match "NZXT Kraken X" set pump speed 20'
+abbr -a Aio 'sudo liquidctl --match "NZXT Kraken X" set pump speed 20'
 abbr -a v vim
