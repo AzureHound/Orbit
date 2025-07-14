ce124b3f0c004f558819054ddc98a6845ce65cff
Wolf <azurehound.landline090@slmail.me>
2025-07-14 14:39:54 +0530

    ♻️ refactor(zsh): code refactoring

--- .config/zsh/compinit.zsh
+++ .config/zsh/compinit.zsh
@@ -1,6 +1,5 @@
 #!/usr/bin/env zsh
 
 # Load Completions
-[[ -d "$XDG_CACHE_HOME/zsh" ]] || mkdir -p "$XDG_CACHE_HOME/zsh"
-autoload -Uz compinit && compinit -d $XDG_CACHE_HOME/zsh/zcompdump
+autoload -Uz compinit && compinit
 zinit cdreplay -q
