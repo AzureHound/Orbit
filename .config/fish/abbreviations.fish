b83f6322b65e51f2ebdd8b32c2074fdfd6bc3218
Wolf <azurehound.landline090@slmail.me>
2025-07-14 14:45:45 +0530

    ♻️ refactor(fish): abbr changed nvim < vim

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -3,4 +3,4 @@ abbr -a check pacman -Qs
 abbr -a d docker
 abbr -a gamedir cd /mnt/Drive/SteamLibrary/steamapps/common
 abbr -a aio 'sudo liquidctl --match "NZXT Kraken X" set pump speed 20'
-abbr -a v nvim
+abbr -a v vim
