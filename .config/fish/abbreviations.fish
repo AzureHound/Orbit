2b5621fa7ad78a6a06bc1f33bec6eae71c69efd0
Wolf <183492338+AzureHound@users.noreply.github.com>
2025-07-15 19:49:01 +0530

    ✨ feat(fish): added abbr for mechism

--- .config/fish/abbreviations.fish
+++ .config/fish/abbreviations.fish
@@ -3,4 +3,5 @@ abbr -a check pacman -Qs
 abbr -a d docker
 abbr -a gamedir cd /mnt/Drive/SteamLibrary/steamapps/common
 abbr -a Aio 'sudo liquidctl --match "NZXT Kraken X" set pump speed 20'
+abbr -a HyperX 'mechsim -V 100 -s cherrymx-black-pbt'
 abbr -a v vim
