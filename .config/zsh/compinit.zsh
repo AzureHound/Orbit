06cf66315d5852541edf7b3c295573c65ad274ea
Wolf <azurehound.landline090@slmail.me>
2025-07-28 08:18:32 +0530

    🩹 quickfix(zsh): zcomdump path changed

--- .config/zsh/compinit.zsh
+++ .config/zsh/compinit.zsh
@@ -1,5 +1,12 @@
 #!/usr/bin/env zsh
 
+local _zcompdump_file="$XDG_CONFIG_HOME/zsh/.zcompdump"
+
 # Load Completions
-autoload -Uz compinit && compinit
+if [[ -z "$_zcompdump_file" || ! -f "$_zcompdump_file" || "$_zcompdump_file" -ot "$ZSH_VERSION" ]]; then
+    autoload -Uz compinit && compinit -u -d "$_zcompdump_file"
+else
+    autoload -Uz compinit && compinit -u -C -d "$_zcompdump_file"
+fi
+
 zinit cdreplay -q
