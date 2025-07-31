0ee0ba2a480ffea25612a126c3adfe13c0df815f
Wolf <azurehound.landline090@slmail.me>
2025-07-31 15:17:06 +0530

    💄 style(bash): changed prompt icon style

--- .config/bash/options.sh
+++ .config/bash/options.sh
@@ -8,7 +8,7 @@ HISTFILESIZE=2000
 shopt -s checkwinsize
 
 # Prompt
-PS1='\[\e[0;36m\]\u\[\e[0;37m\]@\[\e[0;34m\]\h\[\e[0;33m\] \w\[\e[0m\]$(if git rev-parse --is-inside-work-tree &>/dev/null; then echo " \[\e[0;32m\] $(git branch 2>/dev/null | grep "\*" | sed "s/* //")"; fi) \[\e[0m\] '
+PS1='\[\e[0;36m\]\u\[\e[0;37m\]@\[\e[0;34m\]\h\[\e[0;33m\] \w\[\e[0m\]$(if git rev-parse --is-inside-work-tree &>/dev/null; then echo " \[\e[0;32m\] $(git branch 2>/dev/null | grep "\*" | sed "s/* //")"; fi) \[\e[0m\] '
 
 # I-beam Cursor
 echo -ne "\e[5 q"
